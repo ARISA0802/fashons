042c5091c9ca387a689d62ebde55e4fef23d0927
EC2 Default User <ec2-user@ip-172-31-45-174.ap-northeast-1.compute.internal>
2020-02-20 05:10:28 +0000

    マイグレーションファイルの整理

--- db/migrate/20200126081805_remove_post_image_id_from_favorites.rb
+++
@@ -1,5 +0,0 @@
-class RemovePostImageIdFromFavorites < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :favorites, :name, :integer
-  end
-end
--- db/migrate/20200127102253_add_sunnyintroduction_to_rains.rb
+++
@@ -1,5 +0,0 @@
-class AddSunnyintroductionToRains < ActiveRecord::Migration[5.2]
-  def change
-    add_column :rains, :sunnyintroduction, :text
-  end
-end
